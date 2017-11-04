22e0ce138db646a7d9d5beec0cfaa15f00fc1826
Unknown <jeka051@gmail.com>
2017-11-04 12:45:26 -0200

    server side do trabalho de sd

---
+++ src/midiaboxserverside/MidiaBoxServerSide.java
@@ -0,0 +1,16 @@
+package midiaboxserverside;
+
+/**
+ *
+ * @author Jess
+ */
+public class MidiaBoxServerSide {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
