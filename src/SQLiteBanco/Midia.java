550765a55047267becb6fcfa78a21c10673be77e
Unknown <jeka051@gmail.com>
2017-11-26 09:30:30 -0200

    xx

---
+++ src/SQLiteBanco/Midia.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package SQLiteBanco;
+
+/**
+ *
+ * @author Jessica
+ */
+public class Midia {
+    private int id_codigo;
+    private String url;
+    private String nome;
+    private String responsavel;
+    private String tipo;
+    private String descricao;
+
+    public int getId_codigo() {
+        return id_codigo;
+    }
+
+    public void setId_codigo(int id_codigo) {
+        this.id_codigo = id_codigo;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getResponsavel() {
+        return responsavel;
+    }
+
+    public void setResponsavel(String responsavel) {
+        this.responsavel = responsavel;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+    
+    
+    
+}
--- src/SQLiteBanco/UsuarioDAO.java
+++ src/SQLiteBanco/UsuarioDAO.java
@@ -5,6 +5,8 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -43,4 +45,97 @@ public class UsuarioDAO {
         }
         return autenticou;
     }
+    
+    public void insertMidia (String url, String nome, String responsavel, String tipo, String descricao ){
+        Connection conn = null;
+        PreparedStatement pst = null;
+               
+        try {
+            conn = Connect.ConnectDB(); // classe connecttDB da classe SQLiteBanco
+            String sql = "INSERT INTO tb_midia  (URL, nome, responsavel, tipo, descricao) values (?, ?, ?, ?, ?,)"; 
+
+            pst = (PreparedStatement) conn.prepareStatement(sql);
+            pst.setString(1, url);  
+            pst.setString(2, nome);
+            pst.setString(3, responsavel);
+            pst.setString(4, tipo);
+            pst.setString(5, descricao);
+            pst.executeUpdate(); 
+                      
+            pst.close();
+            conn.close();
+        }catch(SQLException ex ){
+            Logger.getLogger(UsuarioDAO.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(UsuarioDAO.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(UsuarioDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+    }
+   
+    public String getUrlVideo (String id_codigo){
+        Connection conn = null;
+        PreparedStatement pst = null;
+        ResultSet rs = null;
+        String url = "";
+        try {
+            conn = Connect.ConnectDB(); // classe connecttDB da classe SQLiteBanco
+            String sql = "SELECT URL FROM tb_midia  WHERE id_codigo=?"; // selecionando todo campo username e passoword 
+
+            pst = (PreparedStatement) conn.prepareStatement(sql);
+            pst.setString(1, id_codigo); 
+            rs = pst.executeQuery(); 
+            
+            if (rs.next()) {
+                url = rs.getString("URL");
+            }
+            rs.close();
+            pst.close();
+            conn.close();
+        }catch(SQLException ex ){
+            Logger.getLogger(UsuarioDAO.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(UsuarioDAO.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(UsuarioDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return url;
+    }
+    
+    public List<Midia> listar() throws SQLException, ClassNotFoundException {
+        List<Midia> midia = new ArrayList<>();
+        Connection conn = null;
+        PreparedStatement pst = null;
+        
+        try {
+            conn = Connect.ConnectDB();
+            try {
+                PreparedStatement stmt = conn. prepareStatement("SELECT * FROM tb_midia");
+                ResultSet rs = stmt.executeQuery();
+                while (rs.next()) {
+                    Midia c = new Midia();
+                    c.setId_codigo(rs.getInt("id_codigo"));
+                    c.setUrl(rs.getString("url"));
+                    c.setNome(rs.getString("nome"));
+                    c.setResponsavel(rs.getString("responsavel"));
+                    c.setTipo(rs.getString("tipo"));
+                    c.setDescricao(rs.getString("descrição"));
+                    midia.add(c);
+                }
+                stmt.close();
+                rs.close();
+            } finally {
+                conn.close();
+            }
+        }catch(SQLException ex ){
+            Logger.getLogger(UsuarioDAO.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(UsuarioDAO.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(UsuarioDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return midia;
+    }
+    
 }
